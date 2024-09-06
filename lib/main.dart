e64c6e3ba3c6f8510aaeaf82965a7e16925e7178
Aaron <aaronpromail@gmail.com>
2024-09-06 13:40:41 +0100

    wrap ElevatedButton with Row widget

--- lib/main.dart
+++ lib/main.dart
@@ -55,11 +55,15 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             ),
           ],
         ),
